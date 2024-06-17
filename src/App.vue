dca16e03d68bf5ca0656e680a99732de2644a062
shilihaoa <lhxhl0303.163.com>
2024-06-17 09:57:24 +0800

    feat(global): init
    init初始化

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import {  RouterView } from 'vue-router'
+</script>
+
+<template>
+  123
+
+  <RouterView />
+</template>
+
+<style  >
+html,body,#app{
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  /* background-color: pink; */
+}
+</style>
